de1719d14c9fb9fe5403ef210309b7e11e814af2
cazz <cazzwastaken@gmail.com>
2024-01-28 17:58:31 +0200

    Added context::view_matrix with signature.

--- src/core/context.h
+++ src/core/context.h
@@ -5,11 +5,13 @@
 struct GameInfo;
 struct PlayerList;
 struct HudInfo;
-struct MouseInfo;
+//struct MouseInfo;
+struct ViewMatrix;
 
 namespace context {
-	inline GameInfo* game_info = nullptr;
-	inline PlayerList* player_list = nullptr;
-	inline HudInfo* hud_info = nullptr;
-	inline MouseInfo* mouse_info = nullptr;
+    inline GameInfo* game_info = nullptr;
+    inline PlayerList* player_list = nullptr;
+    inline HudInfo* hud_info = nullptr;
+    //inline MouseInfo* mouse_info = nullptr;
+    inline ViewMatrix* view_matrix = nullptr;
 }
--- src/core/dllmain.cpp
+++ src/core/dllmain.cpp
@@ -1,86 +1,92 @@
-#include <thread>
-#include <iostream>
-#include <format>
 #include <chrono>
+#include <format>
+#include <iostream>
+#include <thread>
 
-#include <Windows.h>
 #include "hooks/hooks.h"
+#include <Windows.h>
 
 DWORD WINAPI run_crashed(LPVOID instance) {
-	const auto injection_time = std::chrono::high_resolution_clock::now();
+    const auto injection_time = std::chrono::high_resolution_clock::now();
 
-	AllocConsole();
+    AllocConsole();
 
-	FILE* fp;
-	freopen_s(&fp, "CONIN$", "r", stdin);
-	freopen_s(&fp, "CONOUT$", "w", stdout);
-	freopen_s(&fp, "CONOUT$", "w", stderr);
+    FILE* fp;
+    freopen_s(&fp, "CONIN$", "r", stdin);
+    freopen_s(&fp, "CONOUT$", "w", stdout);
+    freopen_s(&fp, "CONOUT$", "w", stderr);
 
-	constexpr auto game_info_sig = "48 8B 05 ? ? ? ? F2 0F 10 4F ? F2 0F 5A C9 F2 0F 10 17";
-	constexpr auto player_list_sig = "48 8B 05 ? ? ? ? 48 8D 14 D0 0F B7 79 08 EB 28";
-	constexpr auto hud_info_sig = "48 8B 0D ? ? ? ? 83 B9 ? ? ? ? ? 7C 4E";
-	constexpr auto mouse_info_sig = "48 8B 0D ? ? ? ? 48 89 88 ? ? ? ? 89 9C 24 ? ? ? ?";
+    constexpr auto game_info_sig = "48 8B 05 ? ? ? ? F2 0F 10 4F ? F2 0F 5A C9 F2 0F 10 17";
+    constexpr auto player_list_sig = "48 8B 15 ? ? ? ? 0F B7 59 08 48 C1 E0 03 31 FF 90";
+    constexpr auto hud_info_sig = "48 8B 05 ? ? ? ? 0F B6 88 ? ? ? ? 69 C9 ? ? ? ? 89 0D ? ? ? ? 66 89 0D ? ? ? ?";
+    //constexpr auto mouse_info_sig = "48 8B 0D ? ? ? ? 48 89 88 ? ? ? ? 89 9C 24 ? ? ? ?";
+    constexpr auto view_matrix_sig = "4C 8D 0D ? ? ? ? 48 89 D9 48 89 FA 41 B8";
 
-	context::game_info = *memory::get_offset<GameInfo**>(memory::scan(game_info_sig), 0x3);
-	context::player_list = memory::get_offset<PlayerList*>(memory::scan(player_list_sig), 0x3);
-	context::hud_info = *memory::get_offset<HudInfo**>(memory::scan(hud_info_sig), 0x3);
-	context::mouse_info = *memory::get_offset<MouseInfo**>(memory::scan(mouse_info_sig), 0x3);
+    context::game_info = *memory::get_offset<GameInfo**>(memory::scan(game_info_sig), 0x3);
+    context::player_list = memory::get_offset<PlayerList*>(memory::scan(player_list_sig), 0x3);
+    context::hud_info = *memory::get_offset<HudInfo**>(memory::scan(hud_info_sig), 0x3);
+    //context::mouse_info = *memory::get_offset<MouseInfo**>(memory::scan(mouse_info_sig), 0x3);
+    context::view_matrix = memory::get_offset<ViewMatrix*>(memory::scan(view_matrix_sig), 0x3);
 
-	std::cout << "[addresses]:\n";
-	std::cout << std::format("- context::game_info {:#x}\n", std::uintptr_t(context::game_info));
-	std::cout << std::format("- context::player_list {:#x}\n", std::uintptr_t(context::player_list));
-	std::cout << std::format("- context::hud_info {:#x}\n", std::uintptr_t(context::hud_info));
-	std::cout << std::format("- context::mouse_info {:#x}\n\n", std::uintptr_t(context::mouse_info));
+    std::cout << "[addresses]:\n";
+    std::cout << std::format("- context::game_info {:#x}\n", std::uintptr_t(context::game_info));
+    std::cout << std::format("- context::player_list {:#x}\n", std::uintptr_t(context::player_list));
+    std::cout << std::format("- context::hud_info {:#x}\n", std::uintptr_t(context::hud_info));
+    //std::cout << std::format("- context::mouse_info {:#x}\n\n", std::uintptr_t(context::mouse_info));
+    std::cout << std::format("- context::view_matrix {:#x}\n", std::uintptr_t(context::view_matrix));
 
-	// get the base address of aces.exe
-	const auto base = std::uintptr_t(GetModuleHandle(NULL));
+    // get the base address of aces.exe
+    const auto base = std::uintptr_t(GetModuleHandle(NULL));
 
-	std::cout << "[offsets]:\n";
-	std::cout << std::format("- context::game_info {:#x}\n", std::uintptr_t(memory::get_offset<GameInfo*>(memory::scan(game_info_sig), 0x3)) - base);
-	std::cout << std::format("- context::player_list {:#x}\n", std::uintptr_t(memory::get_offset<PlayerList*>(memory::scan(player_list_sig), 0x3)) - base);
-	std::cout << std::format("- context::hud_info {:#x}\n", std::uintptr_t(memory::get_offset<HudInfo*>(memory::scan(hud_info_sig), 0x3)) - base);
-	std::cout << std::format("- context::mouse_info {:#x}\n\n", std::uintptr_t(memory::get_offset<MouseInfo*>(memory::scan(mouse_info_sig), 0x3)) - base);
+    std::cout << "[offsets]:\n";
+    std::cout << std::format("- context::game_info {:#x}\n", std::uintptr_t(memory::get_offset<GameInfo*>(memory::scan(game_info_sig), 0x3)) - base);
+    std::cout << std::format("- context::player_list {:#x}\n", std::uintptr_t(memory::get_offset<PlayerList*>(memory::scan(player_list_sig), 0x3)) - base);
+    std::cout << std::format("- context::hud_info {:#x}\n", std::uintptr_t(memory::get_offset<HudInfo*>(memory::scan(hud_info_sig), 0x3)) - base);
+    //std::cout << std::format("- context::mouse_info {:#x}\n\n", std::uintptr_t(memory::get_offset<MouseInfo*>(memory::scan(mouse_info_sig), 0x3)) - base);
+    std::cout << std::format("- context::view_matrix {:#x}\n\n", std::uintptr_t(memory::get_offset<ViewMatrix*>(memory::scan(view_matrix_sig), 0x3)) - base);
 
-	try {
-		menu::create();
-		hooks::create();
-	}
-	catch (const std::exception& error) {
-		MessageBox(nullptr, error.what(), "crashed", MB_ICONERROR);
-		hooks::destroy();
-		menu::destroy_menu();
-		FreeConsole();
-		FreeLibraryAndExitThread(static_cast<HMODULE>(instance), 0);
-	}
-	catch (...) {
-		MessageBox(nullptr, "an unknown  exception occurred", "crashed", MB_ICONERROR);
-		hooks::destroy();
-		menu::destroy_menu();
-		FreeConsole();
-		FreeLibraryAndExitThread(static_cast<HMODULE>(instance), 0);
-	}
+    try {
+        menu::create();
+        hooks::create();
+    }
+    catch (const std::exception& error) {
+        MessageBox(nullptr, error.what(), "crashed", MB_ICONERROR);
+        hooks::destroy();
+        menu::destroy_menu();
+        FreeConsole();
+        FreeLibraryAndExitThread(static_cast<HMODULE>(instance), 0);
+    }
+    catch (...) {
+        MessageBox(nullptr, "an unknown  exception occurred", "crashed", MB_ICONERROR);
+        hooks::destroy();
+        menu::destroy_menu();
+        FreeConsole();
+        FreeLibraryAndExitThread(static_cast<HMODULE>(instance), 0);
+    }
 
-	// wait until the unload key is pressed
-	while (!GetAsyncKeyState(VK_END)) {
-		Sleep(200);
-	}
+    // wait until the unload key is pressed
+    while (!GetAsyncKeyState(VK_END) & 1) {
+        Sleep(200);
+    }
 
-	hooks::destroy();
-	menu::destroy_menu();
+    hooks::destroy();
+    menu::destroy_menu();
 
-	FreeConsole();
-	FreeLibraryAndExitThread(static_cast<HMODULE>(instance), 0);
+    std::cout << "crashed has been unloaded. You can safely close this window.\n";
+
+    FreeConsole();
+    FreeLibraryAndExitThread(static_cast<HMODULE>(instance), 0);
 }
 
 BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID) {
-	if (reason == DLL_PROCESS_ATTACH) {
-		DisableThreadLibraryCalls(instance);
+    if (reason == DLL_PROCESS_ATTACH) {
+        DisableThreadLibraryCalls(instance);
 
-		if (const HANDLE h = CreateThread(nullptr, 0, run_crashed, instance, 0, nullptr); h) {
-			CloseHandle(h);
-		}
-	}
+        if (const HANDLE h = CreateThread(nullptr, 0, run_crashed, instance, 0, nullptr); h) {
+            CloseHandle(h);
+        }
+    }
 
-	return TRUE;
+    return TRUE;
 }
 
